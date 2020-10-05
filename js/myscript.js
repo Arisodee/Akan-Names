acceeb26faeda7da967c86702d13ca83aaea6fd1
Arisodee <arisodee@gmail.com>
2020-10-05 03:01:38 +0300

    Add function getDayofBirth

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,6 @@ let maleNamesArray = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
  
 let femaleNamesArray = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
+function getDayOfBirth(){
+    
+}
