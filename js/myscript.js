8c850175673abc40d6617f48eb446323420fac37
Arisodee <arisodee@gmail.com>
2020-10-05 23:42:46 +0300

    Add missing semicolons

--- js/myscript.js
+++ js/myscript.js
@@ -1,77 +1,71 @@
 let maleNamesArray = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
  
 let femaleNamesArray = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
-    
 
 function getDayOfBirth(){
-    /**
-     * document.getElementById("") => https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
-     */
+    
 
     let dob = document.getElementById("dateofbirth");
 
-    let genderInput = document.getElementById("gender")
+    let genderInput = document.getElementById("gender");
 
-    /**
-     * https://developer.mozilla.org/en-US/docs/Glossary/Control_flow
-     */
+    
     if(!dob.value || dob.value === "" || dob.value.length < 10 || dob.value.length > 11){
-        alert("Please enter Date of Birth in the format DD/MM/YYYY")
+        alert("Please enter Date of Birth in the format DD/MM/YYYY");
         return
     }else{
-        /**
-         * Reference : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-         * The Split function will create an array of the format ["02","03","1990"],
-         * Where the item in index 0 = date, 1 = month, 3 = Year of birth
-         * a quick check to confirm if the user entered the correct information is to check the length of the array
-         */
+        
 
 
-        let dateObj = dob.value.split("/")
-        if(dateObj.length > 3 || dateObj.length  < 3){
-            alert("Please enter Date of Birth in the format DD/MM/YYYY")
-            return
-        }
-        //validate the date
-        let dd = dateObj[0]
+    let dateObj = dob.value.split("/");
+    if(dateObj.length > 3 || dateObj.length  < 3){
+        alert("Please enter Date of Birth in the format DD/MM/YYYY");
+        return
+    }
+        
+    let dd = dateObj[0]
 
-        if(parseInt(dd) <= 1 ||  parseInt(dd) > 31 ){
-            alert("Day must be between 1 and 31")
-            return
-        }
-        //valdate the month
-        let mm = dateObj[1]
-        let yyyy =dateObj[2]
-        //calculate day with the formula
-        debugger
-        let dayOfTheWeek = ( ( (parseInt(yyyy.slice(0,2)/4)) -2*parseInt(yyyy.slice(0,2))-1) + ((5*parseInt(yyyy.slice(2,4))/4) ) + ((26*(parseInt(mm)+1)/10)) + parseInt(dd) ) % 7
+    if(parseInt(dd) <= 1 ||  parseInt(dd) > 31 ){
+        alert("Day must be between 1 and 31");
+        return
+    }
+       
+    let mm = dateObj[1]
+    if(parseInt(mm) <= 1 ||  parseInt(mm) > 12 ){
+        alert("Month must be between 1 and 12");
+        return
+    }
 
-        /**
-         * https://developer.mozilla.org/en-US/docs/Web/API/Console/log
-         */
+    let yyyy =dateObj[2]
+    if(parseInt(yyyy) < 1 ||  parseInt(yyyy) > 2020 ){
+        alert("year must be between 1 and 2020");
+        return
+    }
+    debugger
+    let dayOfTheWeek = ( ( (parseInt(yyyy.slice(0,2)/4)) -2*parseInt(yyyy.slice(0,2))-1) + ((5*parseInt(yyyy.slice(2,4))/4) ) + ((26*(parseInt(mm)+1)/10)) + parseInt(dd) ) % 7
 
-        //console.log("Date Obj", dateObj)
+        
 
-        if(genderInput.value === "0"){
-            alert("Please select a gender")
-            return
-        }
-        // we have the gender and day
-        // for example day = 1
+    if(genderInput.value === "0"){
+        alert("Please select a gender");
+        return
+    }
+        
 
-        let nameOfPerson = ''
-        if(gender.value == 'male'){
-            nameOfPerson = maleNamesArray[parseInt(dayOfTheWeek)]
-        }else{
-            nameOfPerson = femaleNamesArray[parseInt(dayOfTheWeek)]
-        }
+    let nameOfPerson = ''
+    if(gender.value == 'male'){
+        nameOfPerson = maleNamesArray[parseInt(dayOfTheWeek)]
+    }else{
+        nameOfPerson = femaleNamesArray[parseInt(dayOfTheWeek)]
+    }
 
-        if(nameOfPerson == ''){
-            alert("we did get a valid day of the week, please try again")
-            return
-        }
+    if(nameOfPerson == ''){
+        alert("we did get a valid day of the week, please try again");
+        return
+    }
 
-
-        alert("Your name is "+ nameOfPerson)
+    // document.getElementById("nameResult").innerHTML = nameOfPerson
+    alert("Your Akan name is: " + nameOfPerson);
     }
 }
+ 
