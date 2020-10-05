935018ac6824d8bb1126d7716b3c687f71b70e20
Arisodee <arisodee@gmail.com>
2020-10-05 10:42:10 +0300

    Add control flows to validate date of birth entered

--- js/myscript.js
+++ js/myscript.js
@@ -3,5 +3,23 @@ let maleNamesArray = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
 let femaleNamesArray = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
 function getDayOfBirth(){
-    
-}
+    let dob = document.getElementById("dateofbirth");
+
+    if(!dob.value || dob.value === "" || dob.value.length < 10 || dob.value.length > 11){
+        alert("Please enter Date of Birth in the format DD/MM/YYYY")
+        return
+    }
+    else{
+        alert('Proceed to select your gender')
+    }
+    let dateObj = dob.value.split("/")
+        if(dateObj.length > 3 || dateObj.length  < 3){
+        alert("Please enter Date of Birth in the format DD/MM/YYYY")
+        return
+        }
+    let dd = dateObj[0]
+
+    if(parseInt(dd) <= 1 ||  parseInt(dd) > 31 ){
+        alert("Day must be between 1 and 31")
+        return
+        
